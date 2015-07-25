7c5ca8a4d3f9bb587f71fd70573f999e1d79a452
shimoda-tomoaki <shimoda-tomoaki@kayac.com>
2015-07-26 01:58:33 +0900

    データベース関連の文字列をresouceファイルに登録

--- HelloWorld/app/src/main/java/com/example/shimoda_tomoaki/helloworld/DBTools.java
+++ HelloWorld/app/src/main/java/com/example/shimoda_tomoaki/helloworld/DBTools.java
@@ -19,15 +19,15 @@ public class DBTools {
     public static void makeTable(Context context) {
         SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase("data/data/" + context.getPackageName() + "/Sample.db", null);
 
-        db.execSQL("DROP TABLE IF EXISTS category");
-        db.execSQL("CREATE TABLE category "
-                + "(_id INTEGER PRIMARY KEY, category TEXT NOT NULL, password TEXT, isLocked INTEGER DEFAULT 0, isUnpublished INTEGER DEFAULT 0)");
-        db.execSQL("DROP TABLE IF EXISTS url");
-        db.execSQL("CREATE TABLE url "
-                + "(_id INTEGER PRIMARY KEY, categoryId INTEGER NOT NULL, url TEXT NOT NULL, created_date DATETIME DEFAULT CURRENT_TIMESTAMP)");
-        db.execSQL("DROP TABLE IF EXISTS image");
-        db.execSQL("CREATE TABLE image "
-                + "(_id INTEGER PRIMARY KEY, categoryId INTEGER NOT NULL, image BLOB NOT NULL, created_date DATETIME DEFAULT CURRENT_TIMESTAMP)");
+        db.execSQL("DROP TABLE IF EXISTS " + R.string.table_category);
+        db.execSQL("CREATE TABLE " + R.string.table_category
+                + " (" + R.string.column_id + " INTEGER PRIMARY KEY, category TEXT NOT NULL, password TEXT, isLocked INTEGER DEFAULT 0, isUnpublished INTEGER DEFAULT 0)");
+        db.execSQL("DROP TABLE IF EXISTS " + R.string.table_url);
+        db.execSQL("CREATE TABLE " + R.string.table_category
+                + " (" + R.string.column_id + " INTEGER PRIMARY KEY, categoryId INTEGER NOT NULL, url TEXT NOT NULL, created_date DATETIME DEFAULT CURRENT_TIMESTAMP)");
+        db.execSQL("DROP TABLE IF EXISTS " + R.string.table_image);
+        db.execSQL("CREATE TABLE " + R.string.table_image
+                + " (" + R.string.column_id + " INTEGER PRIMARY KEY, categoryId INTEGER NOT NULL, image BLOB NOT NULL, created_date DATETIME DEFAULT CURRENT_TIMESTAMP)");
     }
 
     public boolean isExistCategory(int categoryId, String category) {
